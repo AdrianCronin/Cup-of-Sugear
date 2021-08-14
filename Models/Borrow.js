2ee227224bfdcebca2821f79d6a830cfb6d3f24a
grudgecat <grudgecat@protonmail.com>
2021-08-14 14:02:20 -0700

    added model files and seeds

---
+++ Models/Borrow.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Borrow extends Model {}
+
+Borrow.init(
+  // define columns
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    date_out: {
+      type: DataTypes.DATE,
+    },
+    date_in: {
+      type: DataTypes.DATE,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      //FOREIGN KEY: References Owner PK id 
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    gear_id: {
+      type: DataTypes.INTEGER,
+      //FOREIGN KEY: References Gear PK id 
+      references: {
+        model: 'gear',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'borrow',
+  });
+
+module.exports = Borrow;

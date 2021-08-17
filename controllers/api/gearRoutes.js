fb12e75fa7fbe86485672b1eeff1f4b3d5cbac9a
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-16 19:54:50 -0700

    added find gear routes

--- controllers/api/gearRoutes.js
+++ controllers/api/gearRoutes.js
@@ -1,37 +1,40 @@
 const router = require('express').Router();
 const { Gear } = require('../../models');
 
-// find all items
+// find all gear
 router.get('/', async (req, res) => {
     try {
-        res.json(`Reached path: http://localhost:3001/api/items${req.path} `);
+        const gearData = await Gear.findAll();
+        const gear = gearData.map((item) => item.get({plain: true}));
+
+        res.status(200).json(gear);
     } catch (err) {
         res.status(500).json(err);
     };
 });
 
-// find an item
+// find a piece of gear
 router.get('/:id', async (req, res) => {
     try {
-        res.json(`Reached path: http://localhost:3001/api/items${req.path} `);
+        res.json(`Reached path: http://localhost:3001/api/gear${req.path} `);
     } catch (err) {
         res.status(500).json(err);
     };
 });
 
-// update item put route
+// update gear route
 router.put('/update/:id', async (req, res) => {
     try {
-        res.json(`Reached path: http://localhost:3001/api/items${req.path} `);
+        res.json(`Reached path: http://localhost:3001/api/gear${req.path} `);
     } catch (err) {
         res.status(500).json(err);
     };
 });
 
-// delete item route
+// delete gear route
 router.delete('/delete/:id', async (req, res) => {
     try {
-        res.json(`Reached path: http://localhost:3001/api/items${req.path} `);
+        res.json(`Reached path: http://localhost:3001/api/gear${req.path} `);
     } catch (err) {
         res.status(500).json(err);
     };

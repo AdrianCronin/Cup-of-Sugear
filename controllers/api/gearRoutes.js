e34a64c7fc8ba06ea13104d1415fbe1e1d6e238c
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-17 20:32:01 -0700

    added temporary route for finding a user's owned gear

--- controllers/api/gearRoutes.js
+++ controllers/api/gearRoutes.js
@@ -1,13 +1,28 @@
 const router = require('express').Router();
 const { Gear } = require('../../models');
 
-// find all owned gear for the current user
+// find all gear for a user simulating logic using route params
+// TODO: add logic that finds the currently signed in user's gear
+router.get('/user/:id', async (req, res) => {
+    try {
+        const gearData = await Gear.findAll({
+            where: { user_id: req.params.id}
+        });
+        const gear = gearData.map((item) => item.get({plain: true}));
+
+        res.status(200).json(gear);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+// find all gear
 router.get('/', async (req, res) => {
     try {
-        const ownedGearData = await Gear.findAll();
-        const ownedGear = ownedGearData.map((item) => item.get({plain: true}));
+        const gearData = await Gear.findAll();
+        const gear = gearData.map((item) => item.get({plain: true}));
 
-        res.status(200).json(ownedGear);
+        res.status(200).json(gear);
     } catch (err) {
         res.status(500).json(err);
     };

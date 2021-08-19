ace01a6582db191ebac859f49e4a7ac4fb9da259
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-18 21:41:35 -0700

    mygear route now shows the currently logged in users's gear

--- controllers/api/gearRoutes.js
+++ controllers/api/gearRoutes.js
@@ -1,16 +1,35 @@
 const router = require('express').Router();
-const { Gear } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Gear, Borrow } = require('../../models');
 
 // find all gear for a user simulating logic using random generated user id
 // TODO: add logic that finds the currently signed in user's gear
 router.get('/mygear', async (req, res) => {
     try {
+        const user_id = req.session.user_id;
+        // get all gear for the current user
         const gearData = await Gear.findAll({
-            where: { user_id: Math.floor(Math.random() * 4) + 1}
+            where: { user_id: user_id }
         });
-        const gear = gearData.map((item) => item.get({plain: true}));
+        const gear = gearData.map((item) => item.get({ plain: true }));
 
-        res.render('mygear', {gear});
+        const borrowedResults = await sequelize.query(`
+        SELECT 
+            borrow.id, borrow.date_out, borrow.date_in, borrow.user_id as borrower_id, borrow.gear_id, gear.name AS gear_name, gear.description, gear.user_id as owner_id, CONCAT(user.first_name, " ", user.last_name) as owner_name
+        FROM borrow
+        JOIN gear ON gear.id = borrow.gear_id
+        JOIN user ON gear.user_id = user.id
+        ORDER BY borrow.id;
+        `);
+
+        let borrowedGear = [];
+        borrowedResults[0].forEach(element => {
+            if (element.borrower_id === user_id) {
+                borrowedGear.push(element);
+            }
+        });
+
+        res.render('mygear', {gear, borrowedGear});
     } catch (err) {
         res.status(500).json(err);
     };
@@ -20,7 +39,7 @@ router.get('/mygear', async (req, res) => {
 router.get('/', async (req, res) => {
     try {
         const gearData = await Gear.findAll();
-        const gear = gearData.map((item) => item.get({plain: true}));
+        const gear = gearData.map((item) => item.get({ plain: true }));
 
         res.status(200).json(gear);
     } catch (err) {

0d93fc6e76c259889585b30ae7350f6a43ccbec1
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-20 15:48:09 -0700

    tabbing

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,111 +3,109 @@ const { User } = require('../../models');
 
 // Create new user signup Post route
 router.post('/signup', async (req, res) => {
-    // try {
-        // res.json(`Reached path: http://localhost:3001/api/users${req.path} `);
-            // Find the user who matches the posted e-mail address
-            const userData = await User.findOne({ where: { email: req.body.email } });
-        
-            if (userData) {
-              res
-                .status(400)
-                .json({ message: 'User already exists' });
-              return;
-            }
-            
-            
-          //create new user
-          const newUserData = await User.create(req.body);
+  try {
+    // res.json(`Reached path: http://localhost:3001/api/users${req.path} `);
+    // Find the user who matches the posted e-mail address
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
-          console.log(newUserData);
-      
-            // Create session variables based on the logged in user
-            req.session.save(() => {
-              req.session.user_id = newUserData.id;
-              req.session.logged_in = true;
-              req.session.full_name = `${userData.first_name} ${userData.last_name}`;
-              
-              res.json({ user: newUserData, message: 'Successfully logged in.' });
-            });
-    // } catch (err) {
-    //     res.status(500).json(err);
-    // };
+    if (userData) {
+      res
+        .status(400)
+        .json({ message: 'User already exists' });
+      return;
+    }
+
+    //create new user
+    const newUserData = await User.create(req.body);
+    const newUser = newUserData.get({ plain: true });
+
+    // Create session variables based on the logged in user
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
+      req.session.full_name = `${newUser.first_name} ${newUser.last_name}`;
+
+      res.json({ user: newUser, message: 'Successfully logged in.' });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 // login post route
 router.post('/login', async (req, res) => {
-    try {
-        // res.json(`Reached path: http://localhost:3001/api/users${req.path} `);
-              // Find the user who matches the posted e-mail address
-      const userData = await User.findOne({ where: { email: req.body.email } });
-  
-      if (!userData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      // Verify the posted password with the password store in the database
-      const validPassword = await userData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      // Create session variables based on the logged in user
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-        req.session.full_name = `${userData.first_name} ${userData.last_name}`;
-        
-        res.json({ user: userData, message: 'Successfully logged in.' });
-      });
-    } catch (err) {
-        res.status(500).json(err);
-    };
+  try {
+    // res.json(`Reached path: http://localhost:3001/api/users${req.path} `);
+    // Find the user who matches the posted e-mail address
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // Verify the posted password with the password store in the database
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // Create session variables based on the logged in user
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      req.session.full_name = `${userData.first_name} ${userData.last_name}`;
+
+      res.json({ user: userData, message: 'Successfully logged in.' });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 // logout post route
 router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      // Remove the session variables
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
+  if (req.session.logged_in) {
+    // Remove the session variables
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 // login post route
 router.post('/logout', async (req, res) => {
-    try {
-        res.json(`Reached path: http://localhost:3001/api/users${req.path} `);
-    } catch (err) {
-        res.status(500).json(err);
-    };
+  try {
+    res.json(`Reached path: http://localhost:3001/api/users${req.path} `);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 // update user's password put route
 router.put('/update/:id', async (req, res) => {
-    try {
-        res.json(`Reached path: http://localhost:3001/api/users${req.path} `);
-    } catch (err) {
-        res.status(500).json(err);
-    };
+  try {
+    res.json(`Reached path: http://localhost:3001/api/users${req.path} `);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 // delete user route
 router.delete('/delete/:id', async (req, res) => {
-    try {
-        res.json(`Reached path: http://localhost:3001/api/users${req.path} `);
-    } catch (err) {
-        res.status(500).json(err);
-    };
+  try {
+    res.json(`Reached path: http://localhost:3001/api/users${req.path} `);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,75 +1,73 @@
 const loginFormHandler = async (event) => {
-    // Stop the browser from submitting the form 
-    event.preventDefault();
-  
-    // Get data from form
-    const first_name = document.querySelector('#fname-login').value.trim();
-    const last_name = document.querySelector('#lname-login').value.trim();
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (first_name && last_name && email && password) {
-      // Send the e-mail and password to the server
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ first_name, last_name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in');
-      }
+  // Stop the browser from submitting the form 
+  event.preventDefault();
+
+  // Get data from form
+  const first_name = document.querySelector('#fname-login').value.trim();
+  const last_name = document.querySelector('#lname-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (first_name && last_name && email && password) {
+    // Send the e-mail and password to the server
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ first_name, last_name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in');
     }
-  };
-  
-  const signupFormHandler = async (event) => {
-    // Stop the browser from submitting the form 
-    event.preventDefault();
+  }
+};
 
-    // Gather the data from the form elements on the page
-    const first_name = document.querySelector('#fname-login').value.trim();
-    const last_name = document.querySelector('#lname-login').value.trim();
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+const signupFormHandler = async (event) => {
+  // Stop the browser from submitting the form 
+  event.preventDefault();
 
-    console.log(first_name, last_name, email, password);
+  // Gather the data from the form elements on the page
+  const first_name = document.querySelector('#fname-login').value.trim();
+  const last_name = document.querySelector('#lname-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
-    if (first_name && last_name && email && password) {
-      // Send the e-mail and password to the server
-      const response = await fetch('/api/users/signup', {
-        method: 'POST',
-        body: JSON.stringify({ first_name, last_name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in');
-      }
+  // console.log(first_name, last_name, email, password);
+
+  if (first_name && last_name && email && password) {
+    // Send the e-mail and password to the server
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({ first_name, last_name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in');
     }
-  };
-  
-  if(  document
-    .querySelector('.login-form')) {
-      document
-      .querySelector('.login-form')
-      .addEventListener('submit', loginFormHandler);
-    }
+  }
+};
 
-if(    document
-  .querySelector('.signup-form') 
-
-)
- {
+if (document
+  .querySelector('.login-form')) {
   document
-  .querySelector('.signup-form') 
-  .addEventListener('submit', signupFormHandler);
- }
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+}
+
+if (document
+  .querySelector('.signup-form')
+
+) {
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+}
 
 
 
 
- 

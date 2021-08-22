c97df4bf8cc002a07096b5096d6931042c3b2d1b
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-21 21:11:52 -0700

    removed all instances of date_in and date_out

--- Seeds/seed.js
+++ Seeds/seed.js
@@ -10,22 +10,22 @@ const borrowData = require('./borrowData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-    const users = await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
-    });
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-    const categories = await Category.bulkCreate(categoryData, {
-        returning: true,
-    });
+  const categories = await Category.bulkCreate(categoryData, {
+    returning: true,
+  });
 
-    const gear = await Gear.bulkCreate(gearData, {
-        returning: true,
-    });
+  const gear = await Gear.bulkCreate(gearData, {
+    returning: true,
+  });
 
-    const borrow = await Borrow.bulkCreate(borrowData, {
-      returning: true,
-    });
+  const borrow = await Borrow.bulkCreate(borrowData, {
+    returning: true,
+  });
 
   process.exit(0);
 };
--- controllers/api/gearRoutes.js
+++ controllers/api/gearRoutes.js
@@ -18,7 +18,7 @@ router.get('/mygear', withAuth, async (req, res) => {
         // get all gear current user is borrowing
         const borrowedResults = await sequelize.query(`
         SELECT 
-            borrow.id, borrow.date_out, borrow.date_in, borrow.user_id as borrower_id, borrow.gear_id, gear.name AS gear_name, 
+            borrow.id, borrow.user_id as borrower_id, borrow.gear_id, gear.name AS gear_name, 
             gear.description, gear.user_id as owner_id, CONCAT(user.first_name, " ", user.last_name) as owner_name
         FROM borrow
         JOIN gear ON gear.id = borrow.gear_id

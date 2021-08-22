55f9af69be6af1fb3ca3fa00c4df22396a8f6211
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-21 23:12:44 -0700

    made the mygear views correctly render the data.

--- Utils/helpers.js
+++ Utils/helpers.js
@@ -1,9 +1,15 @@
 module.exports = {
   format_date: (date) => {
+    if(date === null) {
+      return null;
+    }
     // Format date as MM/DD/YYYY
     return date.toDateString();
   },
   return_date: (date) => {
+    if(date === null) {
+      return null;
+    }
     let returnDate = new Date(date);
     returnDate.setDate(returnDate.getDate() + 14);
     return returnDate.toDateString();
--- controllers/api/gearRoutes.js
+++ controllers/api/gearRoutes.js
@@ -11,15 +11,16 @@ router.get('/mygear', withAuth, async (req, res) => {
 
         // get all gear for the current user
         const myGearData = await sequelize.query(`
-        SELECT gear.id, gear.name, gear.description, gear.user_id AS owner_id, gear.category_id,
-            borrow.user_id AS borrower_id, CONCAT(u1.first_name, " ", u1.last_name) as borrower_name, borrow.created_at AS borrow_date
+        SELECT 
+            gear.id AS gear_id, gear.name AS gear_name, gear.description AS gear_description, gear.user_id AS owner_id, gear.category_id,
+            CONCAT(u1.first_name, " ", u1.last_name) AS owner_name,
+            borrow.user_id AS borrower_id, borrow.created_at AS borrow_date,
+            CONCAT(u2.first_name, " ", u2.last_name) AS borrower_name
         FROM gear
-        LEFT JOIN borrow
-        ON borrow.gear_id = gear.id
-        JOIN user u1
-        ON borrow.user_id = u1.id
-        WHERE gear.user_id = 4;
-
+        JOIN user u1 ON gear.user_id = u1.id
+        LEFT JOIN borrow ON borrow.gear_id = gear.id
+        LEFT JOIN user u2 ON borrow.user_id = u2.id
+        WHERE gear.user_id = ${user_id};
         `);
 
         // get all gear current user is borrowing
@@ -40,6 +41,10 @@ router.get('/mygear', withAuth, async (req, res) => {
                 borrowedGear.push(element);
             }
         });
+
+        console.log(`\n\n${(user_id)}\n\n`)
+        console.log(`\n\n${JSON.stringify(myGearData[0])}\n\n`)
+
 
         // get categories to use in add gear form
         const categoryData = await Category.findAll();
